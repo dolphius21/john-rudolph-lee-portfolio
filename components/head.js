3e7b59f213efaa12a675984dbbfd661f06e2e018
dolphius21 <johnrudolphlee@gmail.com>
2021-05-09 11:12:00 +0800

    created a head component

---
+++ components/head.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const HeadComp = () => {
+  return (
+    <Head>
+      <title>John Rudolph Lee</title>
+      <meta
+        name="John Rudolph Lee | Digital Designer & Front-End Web Developer"
+        content="Experienced Digital Designer & Front-End Web Developer"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default HeadComp;
