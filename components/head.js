ba4476fa08da1f0e453bac77da3ad160ce28add0
dolphius21 <johnrudolphlee@gmail.com>
2021-05-13 16:22:16 +0800

    added og meta tags

--- components/head.js
+++ components/head.js
@@ -9,6 +9,20 @@ const HeadComp = () => {
         name="John Rudolph Lee | Digital Designer & Front-End Web Developer"
         content="initial-scale=1.0, width=device-width"
       />
+      <meta
+        property="og:url"
+        content="https://john-rudolph-lee-portfolio.vercel.app/"
+      />
+      <meta
+        property="og:title"
+        content="John Rudolph Lee | Digital Designer & Front-End Web Developer"
+      />
+      <meta property="og:type" content="personal website" />
+      <meta
+        property="og:description"
+        content="Hi, I'm John Rudolph Lee. My 8 years of experience as a Graphic Designer has afforded me a well-rounded skillset, including concept design, project management and communication abilities. I am also learning new skills like web development and just recently completed a coding bootcamp which enabled me to learn new technologies like: HTML, CSS, Javascript, MERN(MongoDB, Express.JS, React.JS, Node.JS)."
+      />
+      <meta property="og:image" content="/website-thumbnail" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
