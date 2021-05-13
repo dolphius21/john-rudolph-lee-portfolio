a479703c5c2067564542432e11e5f658fb0bc23c
dolphius21 <johnrudolphlee@gmail.com>
2021-05-13 16:33:09 +0800

    edited meta descirption

--- components/head.js
+++ components/head.js
@@ -20,7 +20,7 @@ const HeadComp = () => {
       <meta property="og:type" content="personal website" />
       <meta
         property="og:description"
-        content="Hi, I'm John Rudolph Lee. My 8 years of experience as a Graphic Designer has afforded me a well-rounded skillset, including concept design, project management and communication abilities. I am also learning new skills like web development and just recently completed a coding bootcamp which enabled me to learn new technologies like: HTML, CSS, Javascript, MERN(MongoDB, Express.JS, React.JS, Node.JS)."
+        content="Hi, I'm John Rudolph Lee. A front-end web developer/digital designer My 8 years of experience as a Graphic Designer has afforded me a well-rounded skillset, including concept design, project management and communication abilities. I am also knowledgeable in technologies like: HTML, CSS, Javascript, MERN(MongoDB, Express.JS, React.JS, Node.JS)."
       />
       <meta property="og:image" content="/website-thumbnail.jpg" />
       <link rel="icon" href="/favicon.ico" />
