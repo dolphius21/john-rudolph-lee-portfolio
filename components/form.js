b0bf88e7b7dcc42f099124195eade003c742f7c8
dolphius21 <johnrudolphlee@gmail.com>
2021-05-15 13:23:43 +0800

    temporarily used formsubmit

--- components/form.js
+++ components/form.js
@@ -13,15 +13,24 @@ const Form = () => {
         I'm available for work or projects. You can contact me using the form
         below, or by emailing me at johnrudolphlee@gmail.com
       </p>
-      <form onSubmit={(e) => handleSubmit(onSubmitForm)}>
+      <form
+        action="https://formsubmit.co/johnrudolphlee@gmail.com"
+        method="POST"
+      >
         <div className="form-group">
-          <input type="text" placeholder="Name" required />
+          <input type="text" name="name" placeholder="Name..." required />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email" required />
+          <input type="text" name="email" placeholder="Email..." required />
         </div>
         <div className="form-group">
-          <textarea type="text" rows={4} placeholder="Message" required />
+          <textarea
+            type="text"
+            name="message"
+            rows={4}
+            placeholder="Message..."
+            required
+          />
         </div>
         <button className="form-btn" type="submit">
           Submit
